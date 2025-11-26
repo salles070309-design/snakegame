9fc0c6723b676adeaae7f72e2e9ffb65930cbc4d
salles.vm <salles.07.03.09@gmail.com>
2025-11-26 16:16:06 -0300

    Update cobra.js

--- cobra.js
+++ cobra.js
@@ -1,6 +1,6 @@
 const cobra = {
     vida: 3,
-    tamanho: 10,
+    tamanho: 14,
     cor: "black",
     x:400,
     y:260,
@@ -17,6 +17,22 @@ const cobra = {
         if (this.direcao == "esquerda") this.x--;
         if (this.direcao == "cima") this.y--;
         if (this.direcao == "baixo") this.y++;
+        if ((this.x < 0)
+            || (this.x > tela.largura - this.tamanho))
+            this.morrer();
+        if ((this.y < placar.altura)
+            || (this.y > tela.altura + placar.altura
+                - this.tamanho))
+                this.morrer();
+
+
+
+    },
+
+    morrer(){
+        this.vida--;
+        this.x = 400;
+        this.y = 260;
     }
     }
 
