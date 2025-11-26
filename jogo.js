b4ef25ea5d26c2923d9c4e006265a2d17bfb5ad1
salles.vm <salles.07.03.09@gmail.com>
2025-11-26 16:16:42 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -2,14 +2,18 @@ function jogar(){
     placar.desenhar();
     tela.desenhar();
     cobra.desenhar();
+    ratinho.desenhar();
     cobra.mover();
     requestAnimationFrame(jogar)
 }
+let ratinho = new Ratinho();
 jogar();
+
 
 document.addEventListener('keydown',(evento)=>{
     if (evento.key== '8') cobra.direcao='cima';
     if(evento.key== '6') cobra.direcao='direita';
     if(evento.key== '2') cobra.direcao= 'baixo';
     if(evento.key== '4') cobra.direcao= 'esquerda';
+
 })
